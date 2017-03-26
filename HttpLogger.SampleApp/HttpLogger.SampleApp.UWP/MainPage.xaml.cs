bf71672776f72c1ce0c38585e5f470a28d5d9fda
Antonio Feregrino <antonio.feregrino@gmail.com>
2017-03-25 22:40:11 -0600

    All the code.

---
+++ HttpLogger.SampleApp/HttpLogger.SampleApp.UWP/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace HttpLogger.SampleApp.UWP
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            LoadApplication(new SampleApp.App());
+        }
+    }
+}
---
+++ HttpLogger.SampleApp/HttpLogger.SampleApp/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace HttpLogger.SampleApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_OnClicked(object sender, EventArgs e)
+        {
+            var loggingHandler = new HttpLoggingHandler(new HttpClientHandler(), RequestAction, ResponseAction);
+            var c = new HttpClient(loggingHandler);
+
+            var result = await c.GetStringAsync("http://pokeapi.co/api/v2/pokemon/1/");
+        }
+
+        private Task ResponseAction(HttpResponseMessage httpResponseMessage)
+        {
+            Device.BeginInvokeOnMainThread(() => { LabelResponse.Text = httpResponseMessage.ToString(); });
+
+            return Task.FromResult(0);
+        }
+
+        private Task RequestAction(HttpRequestMessage httpRequestMessage)
+        {
+            Device.BeginInvokeOnMainThread(() => { LabelRequest.Text = httpRequestMessage.ToString(); });
+
+            return Task.FromResult(0);
+        }
+    }
+}
